ed178dfaabe9ebc59f189c885413cadcf9446589
minsubro <mmss3265@gmail.com>
2023-02-24 03:54:28 +0900

    fix:norm

--- src/constructor/viewport_construcotr.c
+++ src/constructor/viewport_construcotr.c
@@ -6,7 +6,7 @@
 /*   By: minsukan <minsukan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/08 10:30:24 by minsukan          #+#    #+#             */
-/*   Updated: 2023/02/24 03:48:42 by minsukan         ###   ########.fr       */
+/*   Updated: 2023/02/24 03:54:16 by minsukan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,8 @@ t_viewport	c_viewport(t_camera *camera)
 
 	h = tan(degree_to_radian((camera->fov)) / 2);
 	viewport.height = 2.0 * h;
-	viewport.width = viewport.height * (16.0/9.0);
-	w = v_unit(v_minus_(camera->dir_vector)); 
+	viewport.width = viewport.height * (16.0 / 9.0);
+	w = v_unit(v_minus_(camera->dir_vector));
 	u = v_unit(v_cross(camera->v_up, w));
 	v = v_cross(w, u);
 	viewport.h_vector = v_mult(u, viewport.width);
@@ -33,7 +33,7 @@ t_viewport	c_viewport(t_camera *camera)
 	return (viewport);
 }
 
-t_viewport sub_viewport(t_camera *camera)
+t_viewport	sub_viewport(t_camera *camera)
 {
 	t_viewport	viewport;
 	double		theta;
@@ -43,13 +43,12 @@ t_viewport sub_viewport(t_camera *camera)
 	h = tan(theta / 2);
 	viewport.height = 2.0 * h;
 	viewport.width = viewport.height;
-	
 	viewport.h_vector = c_vector3(viewport.width, 0, 0);
 	viewport.v_vector = c_vector3(0, viewport.height, 0);
 	viewport.left_bottom = \
-			v_minus(v_minus(v_minus(camera->point, v_divide(viewport.h_vector, 2)), \
+			v_minus(v_minus(v_minus(camera->point, \
+			v_divide(viewport.h_vector, 2)), \
 			v_divide(viewport.v_vector, 2)), \
 			c_vector3(0, 0, 1));
 	return (viewport);
 }
-
